eefb1be3b9037551a92629f6e909724200a7e5c5
Louis DeLosSantos <louis.delos@gmail.com>
2022-11-23 19:14:27 -0500

    libbuf: don't try to unload on buffer delete
    Signed-off-by: Louis DeLosSantos <louis.delos@gmail.com>

--- lua/ide/lib/buf.lua
+++ lua/ide/lib/buf.lua
@@ -71,7 +71,7 @@ function Buf.delete_buffer_by_name(name)
     for _, buf in ipairs(vim.api.nvim_list_bufs()) do
         local buf_name = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(buf), ":.")
         if buf_name == name then
-            vim.api.nvim_buf_delete(buf, {force=true, unload=true})
+            vim.api.nvim_buf_delete(buf, {force=true})
         end
     end
 end
