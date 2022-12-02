f9ac9501f8b0f47c0a141f744cec052fe35624e8
Louis DeLosSantos <louis.delos@gmail.com>
2022-12-01 19:31:53 -0500

    Revert "fix lazy-loading by checking if we're already past the 'VimEnter' event"
    This reverts commit 8b9bfa6756d537174a4b7b16095829e7323b252f.

--- lua/ide/workspaces/workspace_controller.lua
+++ lua/ide/workspaces/workspace_controller.lua
@@ -246,29 +246,25 @@ function WorkspaceController.new(config)
         -- issues, so do it only after "VimEnter" fires.
         local restore = libwin.restore_cur_win()
         local init_autocmd = nil
-        local init_callback = function()
-            -- do not assign if we are git tool or manpage reader
-            local buf_name = vim.api.nvim_buf_get_name(0)
-            if vim.startswith(buf_name, 'man://') or string.find(buf_name, '%.git/') then
-                log.info('Not assigning workspace, buffer is git tool or manpage reader.')
-                return
-            end
+        init_autocmd =  vim.api.nvim_create_autocmd({ "VimEnter" }, {
+            callback = function()
+                -- do not assign if we are git tool.
+                local buf_name = vim.api.nvim_buf_get_name(0)
+                if vim.fn.match(buf_name, ".git/*") >= 0 then
+                    return
+                end
+                -- do not assign if we are a man reader.
+                if vim.fn.match(buf_name, "man://*") >= 0 then
+                    return
+                end
 
-            for _, tab in ipairs(vim.api.nvim_list_tabpages()) do
-                assign_ws(tab)
-            end
-            restore()
-            if init_autocmd then
+                for _, tab in ipairs(vim.api.nvim_list_tabpages()) do
+                    assign_ws(tab)
+                end
+                restore()
                 vim.api.nvim_del_autocmd(init_autocmd)
             end
-        end
-        if vim.v.vim_did_enter then
-            vim.defer_fn(init_callback, 1)
-        else
-            init_autocmd =  vim.api.nvim_create_autocmd({ "VimEnter" }, {
-                callback = init_callback,
-            })
-        end
+        })
 
         self.tabnew_autocmd_id = vim.api.nvim_create_autocmd({ "TabNewEntered" }, {
             callback = self.tabnew_autocmd
