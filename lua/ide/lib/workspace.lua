3145ffa4fcbb9ecb364ddf08bc82e6e2d877720e
Louis DeLosSantos <louis.delos@gmail.com>
2022-12-25 18:41:46 -0500

    libworkspace: add workspace library
    Signed-off-by: Louis DeLosSantos <louis.delos@gmail.com>

---
+++ lua/ide/lib/workspace.lua
@@ -0,0 +1,11 @@
+local Workspace = {}
+
+-- Given a @Workspace, determine if nvim-ide is currently opened to it.
+function Workspace.is_current_ws(ws)
+    if ws.tab == vim.api.nvim_get_current_tabpage() then
+        return true
+    end
+    return false
+end
+
+return Workspace
